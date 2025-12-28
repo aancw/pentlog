955b7431e884311eee65aad5f648f404ea5a2b03
Aan <6284204+aancw@users.noreply.github.com>
2025-12-28 22:41:04 +0700

    feat: add reset engagement

---
+++ cmd/reset.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"pentlog/pkg/config"
+
+	"github.com/spf13/cobra"
+)
+
+var resetCmd = &cobra.Command{
+	Use:   "reset",
+	Short: "Clear the current active engagement context",
+	Run: func(cmd *cobra.Command, args []string) {
+		path, err := config.GetContextFilePath()
+		if err != nil {
+			fmt.Printf("Error resolving path: %v\n", err)
+			os.Exit(1)
+		}
+
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			fmt.Println("No active context to reset.")
+			return
+		}
+
+		if err := os.Remove(path); err != nil {
+			fmt.Printf("Error removing context file: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Println("Active context cleared. You can now start a new engagement.")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(resetCmd)
+}
+
