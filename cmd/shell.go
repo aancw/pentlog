d63834e46a58acefd3c39148d50be7438a3a91c1
Aan <6284204+aancw@users.noreply.github.com>
2025-12-30 02:19:40 +0700

    feat: inject PS1 prompt for zsh

--- cmd/shell.go
+++ cmd/shell.go
@@ -73,7 +73,21 @@ var shellCmd = &cobra.Command{
 		c.Stdout = os.Stdout
 		c.Stderr = os.Stderr
 
+		shell := os.Getenv("SHELL")
+		if shell == "" {
+			shell = "/bin/sh"
+		}
+		baseShell := filepath.Base(shell)
+
+		tempDir, err := os.MkdirTemp("", "pentlog-shell-*")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Warning: Could not create temp dir for shell config: %v\n", err)
+		} else {
+			defer os.RemoveAll(tempDir)
+		}
+
 		newEnv := os.Environ()
+
 		newEnv = append(newEnv, fmt.Sprintf("PENTLOG_CLIENT=%s", ctx.Client))
 		newEnv = append(newEnv, fmt.Sprintf("PENTLOG_ENGAGEMENT=%s", ctx.Engagement))
 		newEnv = append(newEnv, fmt.Sprintf("PENTLOG_SCOPE=%s", ctx.Scope))
@@ -82,6 +96,49 @@ var shellCmd = &cobra.Command{
 		newEnv = append(newEnv, fmt.Sprintf("PENTLOG_SESSION_DIR=%s", sessionDir))
 		newEnv = append(newEnv, fmt.Sprintf("PENTLOG_SESSION_METADATA_PATH=%s", metaFilePath))
 		newEnv = append(newEnv, fmt.Sprintf("PENTLOG_SESSION_LOG_PATH=%s", logFilePath))
+
+		promptSegment := fmt.Sprintf("(pentlog:%s/%s)", ctx.Client, ctx.Phase)
+
+		if baseShell == "zsh" && tempDir != "" {
+			zshrcPath := filepath.Join(tempDir, ".zshrc")
+
+			userZshrc := filepath.Join(os.Getenv("HOME"), ".zshrc")
+			zshContent := ""
+			if _, err := os.Stat(userZshrc); err == nil {
+				zshContent += fmt.Sprintf("source %s\n", userZshrc)
+			}
+
+			zshContent += "\n# Pentlog Prompt Injection\n"
+			zshContent += fmt.Sprintf("RPROMPT=\"%%F{cyan}%s%%f $RPROMPT\"\n", promptSegment)
+
+			if err := os.WriteFile(zshrcPath, []byte(zshContent), 0644); err != nil {
+				fmt.Fprintf(os.Stderr, "Warning: Could not write .zshrc: %v\n", err)
+			} else {
+				newEnv = append(newEnv, fmt.Sprintf("ZDOTDIR=%s", tempDir))
+			}
+		} else {
+
+			if baseShell == "bash" && tempDir != "" {
+				bashrcPath := filepath.Join(tempDir, ".bashrc")
+				userBashrc := filepath.Join(os.Getenv("HOME"), ".bashrc")
+				bashContent := ""
+				if _, err := os.Stat(userBashrc); err == nil {
+					bashContent += fmt.Sprintf("source %s\n", userBashrc)
+				}
+				bashContent += fmt.Sprintf("\nPS1=\"\\[\\033[0;36m\\]%s\\[\\033[0m\\] $PS1\"\n", promptSegment)
+
+				if err := os.WriteFile(bashrcPath, []byte(bashContent), 0644); err != nil {
+					fmt.Fprintf(os.Stderr, "Warning: Could not write .bashrc: %v\n", err)
+				} else {
+
+					newEnv = append(newEnv, fmt.Sprintf("PS1=\\[\\033[0;36m\\]%s\\[\\033[0m\\] $PS1", promptSegment))
+				}
+			}
+		}
+
+		c.Stdin = os.Stdin
+		c.Stdout = os.Stdout
+		c.Stderr = os.Stderr
 		c.Env = newEnv
 
 		fmt.Println("Starting RECORDED session...")
@@ -107,10 +164,13 @@ var shellCmd = &cobra.Command{
 
 		if err := c.Run(); err != nil {
 			if exitError, ok := err.(*exec.ExitError); ok {
-				os.Exit(exitError.ExitCode())
+				if exitError.ExitCode() != 0 {
+					os.Exit(exitError.ExitCode())
+				}
+			} else {
+				fmt.Fprintf(os.Stderr, "Error running recorder: %v\n", err)
+				os.Exit(1)
 			}
-			fmt.Fprintf(os.Stderr, "Error running recorder: %v\n", err)
-			os.Exit(1)
 		}
 	},
 }
