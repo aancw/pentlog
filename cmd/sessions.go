cf3dcb188ac03b28be7619b968c2e5dc259378fe
Aan <6284204+aancw@users.noreply.github.com>
2026-01-26 14:21:00 +0700

    feat(sessions): add limit and pagination

--- cmd/sessions.go
+++ cmd/sessions.go
@@ -9,11 +9,24 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	sessionsLimit  int
+	sessionsOffset int
+)
+
 var sessionsCmd = &cobra.Command{
 	Use:   "sessions",
 	Short: "List recorded sessions",
 	Run: func(cmd *cobra.Command, args []string) {
-		sessions, err := logs.ListSessions()
+		var sessions []logs.Session
+		var err error
+
+		if sessionsLimit > 0 || sessionsOffset > 0 {
+			sessions, err = logs.ListSessionsPaginated(sessionsLimit, sessionsOffset)
+		} else {
+			sessions, err = logs.ListSessions()
+		}
+
 		if err != nil {
 			fmt.Printf("Error listing sessions: %v\n", err)
 			os.Exit(1)
@@ -30,9 +43,19 @@ var sessionsCmd = &cobra.Command{
 			fmt.Fprintf(w, "%d\t%s\t%d\t%s\n", s.ID, s.ModTime, s.Size, s.DisplayPath)
 		}
 		w.Flush()
+
+		if sessionsLimit > 0 {
+			fmt.Printf("\nShowing %d session(s)", len(sessions))
+			if sessionsOffset > 0 {
+				fmt.Printf(" (offset: %d)", sessionsOffset)
+			}
+			fmt.Println()
+		}
 	},
 }
 
 func init() {
+	sessionsCmd.Flags().IntVarP(&sessionsLimit, "limit", "l", 0, "Maximum number of sessions to display")
+	sessionsCmd.Flags().IntVarP(&sessionsOffset, "offset", "o", 0, "Number of sessions to skip (for pagination)")
 	rootCmd.AddCommand(sessionsCmd)
 }
