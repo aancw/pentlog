dc8cde99d742259d9ec09d8e8d366971d073fefc
Aan <6284204+aancw@users.noreply.github.com>
2026-02-19 15:30:52 +0700

    feat: update session list show more

--- cmd/sessions.go
+++ cmd/sessions.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"pentlog/pkg/errors"
 	"pentlog/pkg/logs"
+	"pentlog/pkg/utils"
+	"strings"
 	"text/tabwriter"
 
 	"github.com/spf13/cobra"
@@ -15,6 +17,8 @@ var (
 	sessionsOffset int
 )
 
+const defaultSessionsPageSize = 20
+
 var sessionsCmd = &cobra.Command{
 	Use:   "sessions",
 	Short: "List recorded sessions",
@@ -24,31 +28,59 @@ var sessionsCmd = &cobra.Command{
 
 		if sessionsLimit > 0 || sessionsOffset > 0 {
 			sessions, err = logs.ListSessionsPaginated(sessionsLimit, sessionsOffset)
-		} else {
-			sessions, err = logs.ListSessions()
-		}
+			if err != nil {
+				errors.DatabaseErr("list sessions", err).Fatal()
+			}
 
-		if err != nil {
-			errors.DatabaseErr("list sessions", err).Fatal()
-		}
+			if len(sessions) == 0 {
+				fmt.Println("No sessions found.")
+				return
+			}
 
-		if len(sessions) == 0 {
-			fmt.Println("No sessions found.")
+			printSessionsTable(sessions)
+
+			if sessionsLimit > 0 {
+				fmt.Printf("\nShowing %d session(s)", len(sessions))
+				if sessionsOffset > 0 {
+					fmt.Printf(" (offset: %d)", sessionsOffset)
+				}
+				fmt.Println()
+			}
+
 			return
 		}
 
-		w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
-		fmt.Fprintln(w, "ID\tTIME\tSIZE\tFILE")
-		for _, s := range sessions {
-			fmt.Fprintf(w, "%d\t%s\t%d\t%s\n", s.ID, s.ModTime, s.Size, s.DisplayPath)
-		}
-		w.Flush()
-
-		if sessionsLimit > 0 {
-			fmt.Printf("\nShowing %d session(s)", len(sessions))
-			if sessionsOffset > 0 {
-				fmt.Printf(" (offset: %d)", sessionsOffset)
+		offset := 0
+		for {
+			sessions, err = logs.ListSessionsPaginated(defaultSessionsPageSize+1, offset)
+			if err != nil {
+				errors.DatabaseErr("list sessions", err).Fatal()
 			}
+
+			if len(sessions) == 0 {
+				if offset == 0 {
+					fmt.Println("No sessions found.")
+				}
+				return
+			}
+
+			hasMore := len(sessions) > defaultSessionsPageSize
+			if hasMore {
+				sessions = sessions[:defaultSessionsPageSize]
+			}
+
+			printSessionsTable(sessions)
+
+			if !hasMore {
+				return
+			}
+
+			answer := utils.PromptString("Show more? (Y/n)", "Y")
+			if !strings.EqualFold(strings.TrimSpace(answer), "y") {
+				return
+			}
+
+			offset += defaultSessionsPageSize
 			fmt.Println()
 		}
 	},
@@ -58,4 +90,13 @@ func init() {
 	sessionsCmd.Flags().IntVarP(&sessionsLimit, "limit", "l", 0, "Maximum number of sessions to display")
 	sessionsCmd.Flags().IntVarP(&sessionsOffset, "offset", "o", 0, "Number of sessions to skip (for pagination)")
 	rootCmd.AddCommand(sessionsCmd)
+}
+
+func printSessionsTable(sessions []logs.Session) {
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
+	fmt.Fprintln(w, "ID\tTIME\tSIZE\tFILE")
+	for _, s := range sessions {
+		fmt.Fprintf(w, "%d\t%s\t%d\t%s\n", s.ID, s.ModTime, s.Size, s.DisplayPath)
+	}
+	w.Flush()
 }
